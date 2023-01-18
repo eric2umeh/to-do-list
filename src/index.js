e6eec91e3e5209c6a32b233f6fe80300ec77809e
Eric <eric2umeh@yahoo.com>
2023-01-18 05:37:57 +0100

    Update array todo task & styling, create module

--- src/index.js
+++ src/index.js
@@ -1,66 +1,41 @@
+import Crud from './moudule/crud.js';
 import './style.css';
 
-const taskInput = document.querySelector('.taskInput input');
-const taskBox = document.querySelector('.taskBox');
+const taskArray = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 1,
+  },
+];
 
-let description = [];
-const completed = false;
-let index = 0;
+function component() {
+  const divElem = document.createElement('div');
+  const inputElem = document.createElement('input');
+  const ulElem = document.createElement('ul');
+  const btnClearCom = document.createElement('button');
 
-// Display task
-function displayTodo() {
-  let liTag = '';
-  description.forEach((element) => {
-    if (element.completed === true) {
-      liTag += `<li class="task">
-        <input type="checkbox" class="checkMe" checked id="check${element.index}" onclick="CheckMe(${element.index});">
-        <input type="text" value="${element.description}" class="listItem" id="item${element.index}" readonly>
+  divElem.classList.add('div-todo');
+  inputElem.placeholder = 'Add to your list...';
+  btnClearCom.classList.add('btn-clear-com');
+  btnClearCom.textContent = 'Clear all completed';
+  btnClearCom.disabled = true;
 
-        <i class="fa-solid fa-ellipsis-vertical" id="edit${element.index}" onclick="editItem(${element.index});"></i>
-        <i class="fa-solid fa-floppy-disk save hide" id="save${element.index}" onclick="saveItem(${element.index});"></i>
-      </li>`;
-    } else {
-      liTag += `<li class="task">
-        <input type="checkbox" class="checkMe" id="check${element.index}" onclick="CheckMe(${element.index});">
-        <input type="text" value="${element.description}" class="listItem" id="item${element.index}" readonly>
-
-        <i class="fa-solid fa-ellipsis-vertical" id="edit${element.index}" onclick="editItem(${element.index});"></i>
-        <i class="fa-solid fa-floppy-disk save hide" id="save${element.index}" onclick="saveItem(${element.index});"></i>
-      </li>`;
-    }
+  taskArray.map((tasks) => {
+    ulElem.appendChild(Crud(tasks));
+    return 'done';
   });
 
-  taskBox.innerHTML = liTag;
-  taskInput.value = '';
+  divElem.append(inputElem, ulElem, btnClearCom);
+
+  return divElem;
 }
 
-// Input Task
-taskInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter' && taskInput.value.length !== 0) {
-    const storedList = localStorage.getItem('todoList');
+const all = document.querySelector('.all');
 
-    if (storedList === null) {
-      description = [];
-    } else {
-      description = JSON.parse(storedList);
-      index = description.length === 0 ? 0 : description.length;
-    }
-
-    const LocalStore = {
-      index,
-      description: taskInput.value,
-      completed,
-    };
-    description.push(LocalStore);
-    localStorage.setItem('todoList', JSON.stringify(description));
-    displayTodo();
-  }
-});
-
-// Page load
-window.onload = () => {
-  if (localStorage.getItem('todoList')) {
-    description = JSON.parse(localStorage.getItem('todoList'));
-  }
-  displayTodo();
-};
+all.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -30,4 +30,5 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
   },
+  mode: 'development',
 };
