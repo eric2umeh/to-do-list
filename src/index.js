6de9725828b9b71d534edde65e41b679dfa0eacd
Eric <eric2umeh@yahoo.com>
2023-01-19 08:32:34 +0100

    Implement add, delete and edit function

--- src/index.js
+++ src/index.js
@@ -1,41 +1,48 @@
-import Crud from './moudule/crud.js';
+import Task from './moudule/crud.js';
 import './style.css';
 
-const taskArray = [
-  {
-    description: 'wash the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'complete To Do list project',
-    completed: false,
-    index: 1,
-  },
-];
+const objTask = new Task();
+const inTask = {};
 
-function component() {
-  const divElem = document.createElement('div');
-  const inputElem = document.createElement('input');
-  const ulElem = document.createElement('ul');
-  const btnClearCom = document.createElement('button');
-
-  divElem.classList.add('div-todo');
-  inputElem.placeholder = 'Add to your list...';
-  btnClearCom.classList.add('btn-clear-com');
-  btnClearCom.textContent = 'Clear all completed';
-  btnClearCom.disabled = true;
-
-  taskArray.map((tasks) => {
-    ulElem.appendChild(Crud(tasks));
-    return 'done';
-  });
-
-  divElem.append(inputElem, ulElem, btnClearCom);
-
-  return divElem;
+if (localStorage.savedTasks) {
+  objTask.tasks = JSON.parse(localStorage.getItem('savedTasks'));
 }
 
 const all = document.querySelector('.all');
+const ulElement = document.querySelector('.ulElement');
+const inputElement = document.querySelector('.inputElement');
+const btnCom = document.createElement('button');
+btnCom.innerHTML = 'Clear all completed';
+btnCom.classList.add('btnCom');
 
-all.appendChild(component());
+inputElement.addEventListener('change', () => {
+  inTask.description = inputElement.value;
+  inTask.completed = false;
+  inTask.index = objTask.tasks.length;
+  objTask.addTask(new Task(inTask.description, inTask.completed, inTask.index));
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    inputElement.value = '';
+  }
+});
+
+btnCom.addEventListener('click', () => {
+  const result = objTask.tasks.filter((task) => task.completed === false);
+  objTask.tasks = result;
+  objTask.populateField();
+  ulElement.innerHTML = '';
+  all.innerHTML = `
+    <div class="title">
+      <p>Today's To Do</p><i class="fas fa-sync-alt"></i>
+    </div>
+    <div class="containerTodo">
+      <ul class='ulElement'></ul>
+    </div>
+  `;
+  all.append(objTask.showTasks(), btnCom);
+});
+
+objTask.showTasks();
+all.append(btnCom);
--- src/moudule/crud.js
+++ src/moudule/crud.js
@@ -1,46 +1,144 @@
-function Crud(task) {
-  let isSelected = false;
-  const itemTaskElem = document.createElement('div');
-  itemTaskElem.classList.add('item-element');
-  const li = document.createElement('li');
-  const label = document.createElement('label');
+const containerElement = document.querySelector('.containerTodo');
+const ulElement = document.querySelector('.ulElement');
 
-  const iconTask = document.createElement('div');
-  iconTask.classList.add('icon-task-container');
-  iconTask.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
-  const iconDel = document.createElement('div');
-  iconDel.innerHTML = '<i class="fas fa-trash-alt"></i>';
+const Task = class {
+  constructor(description, completed = false, index = 1) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+    this.tasks = [
+      {
+        description: 'wash the dogs',
+        completed: false,
+        index: 0,
+      },
+      {
+        description: 'Complete To Do list project',
+        completed: false,
+        index: 1,
+      },
+    ];
+  }
 
-  const checkboxContainer = document.createElement('div');
-  checkboxContainer.classList.add('checkboxContainer');
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.id = `check${task.index}`;
+  populateField = () => {
+    let i = -1;
+    this.tasks.forEach((task) => {
+      i += 1;
+      task.index = i;
+    });
+    localStorage.setItem('savedTasks', JSON.stringify(this.tasks));
+  };
 
-  label.htmlFor = `check${task.index}`;
-  label.innerText = task.description;
+  addTask = (newTask) => {
+    this.tasks.push(newTask);
+    this.populateField();
+    this.showTasks();
+  };
 
-  label.addEventListener('click', () => {
-    isSelected = !isSelected;
-    if (isSelected === true) {
-      label.style.textDecoration = 'line-through';
-    } else {
-      label.style.textDecoration = 'none';
-    }
-  });
+  removeTask(task) {
+    const result = this.tasks.filter((b) => b !== task);
+    this.tasks = result;
+    this.populateField();
+  }
 
-  checkbox.addEventListener('click', () => {
-    if (checkbox.checked) {
-      label.style.textDecoration = 'line-through';
-    } else {
-      label.style.textDecoration = 'none';
-    }
-  });
+  showTasks = () => {
+    ulElement.innerHTML = '';
+    this.tasks.map((task) => {
+      const li = document.createElement('li');
+      const itemElement = document.createElement('div');
+      const checkbox = document.createElement('input');
+      const checkboxContainer = document.createElement('div');
+      checkboxContainer.classList.add('checkboxContainer');
+      const label = document.createElement('label');
+      const icon = document.createElement('div');
+      const textInput = document.createElement('input');
+      textInput.classList.add('textInput', 'hidden');
+      icon.classList.add('iconContainer');
+      itemElement.classList.add('itemElement');
+      icon.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+      const iconMenu = '<i class="fas fa-ellipsis-v"></i>';
+      const iconDelete = '<i class="fas fa-trash-alt"></i>';
+      textInput.value = task.description;
+      checkbox.type = 'checkbox';
+      checkbox.id = `check${task.index}`;
+      label.innerHTML = task.description;
+      checkbox.checked = task.completed;
 
-  checkboxContainer.append(checkbox, label);
-  itemTaskElem.append(checkboxContainer, iconTask);
-  li.appendChild(itemTaskElem);
-  return li;
-}
+      if (checkbox.checked) {
+        label.style.textDecoration = 'line-through';
+      }
 
-export default Crud;
+      label.addEventListener('click', () => {
+        label.classList.add('hidden');
+        textInput.classList.remove('hidden');
+        li.classList.add('editing');
+        icon.innerHTML = iconDelete;
+        icon.style.cursor = 'pointer';
+        textInput.focus();
+      });
+
+      textInput.addEventListener('focus', () => {
+        this.populateField();
+        icon.innerHTML = iconDelete;
+        icon.style.cursor = 'pointer';
+      });
+
+      textInput.addEventListener('change', (e) => {
+        task.description = e.target.value;
+        label.innerHTML = task.description;
+        this.populateField();
+        icon.innerHTML = iconDelete;
+        icon.style.cursor = 'pointer';
+      });
+
+      textInput.addEventListener('blur', () => {
+        label.classList.remove('hidden');
+        textInput.classList.add('hidden');
+        li.classList.remove('editing');
+        setTimeout(() => {
+          icon.innerHTML = iconMenu;
+          icon.style.cursor = 'menu';
+        }, 150);
+      });
+
+      checkbox.addEventListener('click', () => {
+        if (checkbox.checked) {
+          label.style.textDecoration = 'line-through';
+          task.completed = checkbox.checked;
+          this.populateField();
+        } else {
+          label.style.textDecoration = 'none';
+          task.completed = checkbox.checked;
+          this.populateField();
+        }
+      });
+
+      icon.addEventListener('click', () => {
+        if (icon.innerHTML === iconDelete) {
+          this.removeTask(task);
+          ulElement.removeChild(li);
+        }
+      });
+
+      li.addEventListener('keyup', (e) => {
+        if (e.key === 'Enter') {
+          checkbox.focus();
+          textInput.classList.add('hidden');
+          label.classList.remove('hidden');
+          li.classList.remove('editing');
+        }
+      });
+
+      checkboxContainer.append(checkbox, label, textInput);
+      itemElement.append(checkboxContainer, icon);
+
+      li.appendChild(itemElement);
+      ulElement.appendChild(li);
+      return ulElement;
+    });
+    containerElement.appendChild(ulElement);
+    return containerElement;
+  };
+};
+
+export default Task;
